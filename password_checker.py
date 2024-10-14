73bf5b4ffe7e761ce31dab50d75f2978953186ca
Bianca Angelis <diangelis879@outlook.com>
2024-10-14 13:15:22 -0300

    (feat) get first 5 digits of hashed password

--- password_checker.py
+++ password_checker.py
@@ -6,9 +6,13 @@ def password_hasher(user_input):
   hash = sha1(password.encode('utf-8'))
   return hash.hexdigest().upper()
 
+def get_first_five_digits(hashed_password):
+  shortened_hash = hashed_password[:5]
+  return shortened_hash
+
 
 if __name__ == "__main__":
   passwords = sys.argv[1]
-  new_passcode = password_hasher(passwords)
+  new_passcode = get_first_five_digits(passwords)
 
   print(new_passcode)
