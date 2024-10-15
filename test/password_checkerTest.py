dfa39742caf21ca59a816285b45fa939db7f6df9
Bianca Angelis <diangelis879@outlook.com>
2024-10-15 19:06:50 -0300

    (update): tests to use the same test input

--- test/password_checkerTest.py
+++ test/password_checkerTest.py
@@ -4,18 +4,17 @@ sys.path.append('..\password-checker')
 import password_checker
 
 class password_hasher (unittest.TestCase):
+  test_input = "password123"
   
   def test_if_password_received_is_correctly_hashed(self):
     expected_value = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
-    test_input = "password123"
-    actual_result = password_checker.password_hasher(test_input)
+    actual_result = password_checker.password_hasher(self.test_input)
 
     self.assertEqual(expected_value, actual_result)
 
   def test_if_first_five_digits_are_returned(self):
     expected_value = "CBFDA"
-    test_input = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
-    actual_result = password_checker.get_first_five_digits(test_input)
+    actual_result = password_checker.get_first_five_digits(self.test_input)
 
     self.assertEqual(expected_value, actual_result)
 
