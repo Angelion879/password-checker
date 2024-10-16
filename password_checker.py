36de847b3d756adbf84006ed7c034bea612973c1
Bianca Angelis <diangelis879@outlook.com>
2024-10-16 16:05:41 -0300

    (refactor): no direct connection between functions

--- password_checker.py
+++ password_checker.py
@@ -7,19 +7,20 @@ def password_hasher(user_input):
   hash = sha1(password.encode('utf-8'))
   return hash.hexdigest().upper()
 
-def get_first_five_hashed_characters(user_password):
-  shortened_hash = password_hasher(user_password)[:5]
+def get_first_five_hashed_characters(hashed_password):
+  shortened_hash = hashed_password[:5]
   return shortened_hash
 
-def api_url_constructor(user_password):
+def api_url_constructor(short_hashcode):
   base_url = 'https://api.pwnedpasswords.com/range/'
-  shortened_hash = get_first_five_hashed_characters(user_password)
-
-  return f'{base_url}{shortened_hash}'
+  return f'{base_url}{short_hashcode}'
 
 if __name__ == "__main__":
   passwords = sys.argv[1]
-  url = api_url_constructor(passwords)
+  hashed_password = password_hasher(passwords)
+  shortened = get_first_five_hashed_characters(hashed_password)
+
+  url = api_url_constructor(shortened)
   res = requests.get(url)
 
   print(res)
--- test/password_checkerTest.py
+++ test/password_checkerTest.py
@@ -4,23 +4,25 @@ sys.path.append('..\password-checker')
 import password_checker
 
 class password_check (unittest.TestCase):
-  test_input = "password123"
   
   def test_if_password_received_is_correctly_hashed(self):
     expected_value = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
-    actual_result = password_checker.password_hasher(self.test_input)
+    test_input = "password123"
+    actual_result = password_checker.password_hasher(test_input)
 
     self.assertEqual(expected_value, actual_result)
 
   def test_if_first_five_digits_are_returned(self):
     expected_value = "CBFDA"
-    actual_result = password_checker.get_first_five_hashed_characters(self.test_input)
+    test_input = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
+    actual_result = password_checker.get_first_five_hashed_characters(test_input)
 
     self.assertEqual(expected_value, actual_result)
 
   def test_if_url_built_correctly(self):
     expected_value = 'https://api.pwnedpasswords.com/range/CBFDA'
-    actual_result = password_checker.api_url_constructor(self.test_input)
+    test_input = "CBFDA"
+    actual_result = password_checker.api_url_constructor(test_input)
 
     self.assertEqual(expected_value, actual_result)
 
