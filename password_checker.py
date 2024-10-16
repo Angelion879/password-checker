495d6ca8f10c568f8bf5c155b592e826a43a1ae4
Bianca Angelis <diangelis879@outlook.com>
2024-10-16 12:06:11 -0300

    (feat): url constructor with hashed password

--- password_checker.py
+++ password_checker.py
@@ -6,10 +6,15 @@ def password_hasher(user_input):
   hash = sha1(password.encode('utf-8'))
   return hash.hexdigest().upper()
 
-def get_first_five_digits(hashed_password):
-  shortened_hash = password_hasher(hashed_password)[:5]
+def get_first_five_digits(user_password):
+  shortened_hash = password_hasher(user_password)[:5]
   return shortened_hash
 
+def api_url_constructor(user_password):
+  base_url = 'https://api.pwnedpasswords.com/range/'
+  shortened_hash = get_first_five_digits(user_password)
+
+  return f'{base_url}{shortened_hash}'
 
 if __name__ == "__main__":
   passwords = sys.argv[1]
