6a4fc7c3e7769749f56e0bb2895d176cdf78b296
Bianca Angelis <diangelis879@outlook.com>
2024-10-30 12:35:47 -0300

    (update) move request to api to its own function

--- password_checker.py
+++ password_checker.py
@@ -5,22 +5,28 @@ from hashlib import sha1
 def password_hasher(user_input):
   password = str(user_input)
   hash = sha1(password.encode('utf-8'))
+
   return hash.hexdigest().upper()
 
 def get_first_five_hashed_characters(hashed_password):
   shortened_hash = hashed_password[:5]
+  
   return shortened_hash
 
-def api_url_constructor(short_hashcode):
-  base_url = 'https://api.pwnedpasswords.com/range/'
-  return f'{base_url}{short_hashcode}'
+def request_api_data(hash_word):
+  short_hashcode = get_first_five_hashed_characters(hash_word)
+  request_url = f'https://api.pwnedpasswords.com/range/{short_hashcode}'
+
+  res = requests.get(request_url)
+
+  if res.status_code != 200:
+    raise RuntimeError(f'Connection error, status {res.status_code}. Check API and try again.')
+  
+  return res.status_code
 
 if __name__ == "__main__":
   passwords = sys.argv[1]
   hashed_password = password_hasher(passwords)
-  shortened = get_first_five_hashed_characters(hashed_password)
+  response_code = request_api_data(hashed_password)
 
-  url = api_url_constructor(shortened)
-  res = requests.get(url)
-
-  print(res)
+  print(response_code)
