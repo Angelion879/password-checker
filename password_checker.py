9fcad958fed6de00e001976de9f54f06638aaead
Angelion879 <diangelis879@outlook.com>
2024-11-05 15:59:13 -0300

    (refactor) fitting code into pylint patterns

--- password_checker.py
+++ password_checker.py
@@ -1,32 +1,37 @@
 import sys
-import requests
 from hashlib import sha1
+import requests
+
 
 def password_hasher(user_input):
-  password = str(user_input)
-  hash = sha1(password.encode('utf-8'))
+    password = str(user_input)
+    hashed = sha1(password.encode('utf-8'))
 
-  return hash.hexdigest().upper()
+    return hashed.hexdigest().upper()
+
 
 def get_first_five_hashed_characters(hashed_password):
-  shortened_hash = hashed_password[:5]
-  
-  return shortened_hash
+    shortened_hash = hashed_password[:5]
+
+    return shortened_hash
+
 
 def request_api_data(hash_word):
-  short_hashcode = get_first_five_hashed_characters(hash_word)
-  request_url = f'https://api.pwnedpasswords.com/range/{short_hashcode}'
+    short_hashcode = get_first_five_hashed_characters(hash_word)
+    request_url = f'https://api.pwnedpasswords.com/range/{short_hashcode}'
 
-  res = requests.get(request_url)
+    res = requests.get(request_url)
 
-  if res.status_code != 200:
-    raise RuntimeError(f'Connection error, status {res.status_code}. Check API and try again.')
-  
-  return res.status_code
+    if res.status_code != 200:
+        raise RuntimeError(f'Connection error, status {
+                           res.status_code}. Check API and try again.')
+
+    return res.status_code
+
 
 if __name__ == "__main__":
-  passwords = sys.argv[1]
-  hashed_password = password_hasher(passwords)
-  response_code = request_api_data(hashed_password)
+    passwords = sys.argv[1]
+    HASHED = password_hasher(passwords)
+    response_code = request_api_data(HASHED)
 
-  print(response_code)
+    print(response_code)
--- test/password_checkerTest.py
+++ test/password_checkerTest.py
@@ -1,30 +1,32 @@
+import password_checker
 import unittest
 import sys
-sys.path.append('..\password-checker')
-import password_checker
+sys.path.append(r'..\password-checker')
+
 
 class password_check (unittest.TestCase):
-  
-  def test_if_password_received_is_correctly_hashed(self):
-    expected_value = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
-    test_input = "password123"
-    actual_result = password_checker.password_hasher(test_input)
 
-    self.assertEqual(expected_value, actual_result)
+    def test_if_password_received_is_correctly_hashed(self):
+        expected_value = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
+        test_input = "password123"
+        actual_result = password_checker.password_hasher(test_input)
 
-  def test_if_first_five_digits_are_returned(self):
-    expected_value = "CBFDA"
-    test_input = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
-    actual_result = password_checker.get_first_five_hashed_characters(test_input)
+        self.assertEqual(expected_value, actual_result)
 
-    self.assertEqual(expected_value, actual_result)
+    def test_if_first_five_digits_are_returned(self):
+        expected_value = "CBFDA"
+        test_input = "CBFDAC6008F9CAB4083784CBD1874F76618D2A97"
+        actual_result = password_checker.get_first_five_hashed_characters(
+            test_input)
 
-  def test_if_its_connecting_to_api(self):
-    expected_value = 200
-    test_input = "CBFDA"
-    actual_result = password_checker.request_api_data(test_input)
+        self.assertEqual(expected_value, actual_result)
 
-    self.assertEqual(expected_value, actual_result)
+    def test_if_its_connecting_to_api(self):
+        expected_value = 200
+        test_input = "CBFDA"
+        actual_result = password_checker.request_api_data(test_input)
+
+        self.assertEqual(expected_value, actual_result)
 
 
 if __name__ == '__main__':
